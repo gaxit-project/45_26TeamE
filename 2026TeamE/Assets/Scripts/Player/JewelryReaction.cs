b2164a5e3cb5296105ea9af0c9ccf2a0c3ad0f37
Falcon-8810 <hayate.moriyama@icloud.com>
2026-04-22 17:41:53 +0900

    add:宝石取得音の追加

--- 2026TeamE/Assets/Scripts/Player/JewelryReaction.cs
+++ 2026TeamE/Assets/Scripts/Player/JewelryReaction.cs
@@ -50,7 +50,7 @@ public class JewelryReaction : MonoBehaviour
     {
         MoneyManager.Instance.MoneyOnHandIncrease(300000);
         Instantiate(EfectPrefab, transform.position, Quaternion.Euler(-90,0,0));
-        //SoundManager.Instance.PlaySE("•óÎ“üè");
+        SoundManager.Instance.PlaySE("•óÎ“üè");
         Destroy(gameObject);
     }
 }
